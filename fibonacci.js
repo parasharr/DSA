73c58ee3e68020643d1fc85edd7ba6fde1961e61
PranjeetGoswami <prgoswami2020@gmail.com>
2025-01-12 00:14:23 +0530

    push to repo

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+// Give a number "n", find the first "n" elements of the fibonacci seq
+
+function fibonacci(n) {
+  const fib = [0,1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(7));
+
+
+// Big-O = O(n)
